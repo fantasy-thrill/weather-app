f5eadf83e25093607e6035a16d8dfba23a23c983
Ryan Doyley <ryan111267@gmail.com>
2023-09-05 13:43:42 -0400

    UI updates

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,13 +5,26 @@ import { Card } from 'semantic-ui-react'
 function CardExampleCard({weatherData}) {
   const weatherDescription = weatherData.weather[0].description;
   const capitalized = weatherDescription.replace(weatherDescription[0], weatherDescription[0].toUpperCase())
+  const fahrenheit = Math.round((weatherData.main.temp * 1.8) + 32)
+  const feelsLike = Math.round((weatherData.main.feels_like * 1.8) + 32)
+  const windSpeed = Math.round(2.23694 * weatherData.wind.speed)
 
   return (
     <Card>
       <Card.Content>
-          <Card.Header className="header">City: {weatherData.name}</Card.Header>
-          <p>Temprature: {weatherData.main.temp}</p>
-          <p>Description: {capitalized}</p>
+          <Card.Header className="header">{weatherData.name}</Card.Header>
+          <div>
+            <img 
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+              alt=""
+              id="weather-icon" 
+            />
+            <p>{capitalized}</p>
+          </div>
+          <h2 id="temperature">{fahrenheit + "\u00B0F"}</h2>             
+          <p>Feels Like: {feelsLike + "\u00B0F"}</p>        
+          <p>Humidity: {weatherData.main.humidity + "%"}</p>
+          <p>Wind speed: {windSpeed + " mph"}</p>
       </Card.Content>
     </Card>
    )
