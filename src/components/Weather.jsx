c29c51a9ca5e8229261a353953ee5a1f539f32d9
Ryan Doyley <ryan111267@gmail.com>
2023-09-06 12:26:45 -0400

    Added background color effects

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 import { Card } from 'semantic-ui-react'
 
 function CardExampleCard({weatherData}) {
+  const [periodOfDay, setPeriodOfDay] = useState("")
+
   const weatherDescription = weatherData.weather[0].description;
   const capitalized = weatherDescription.replace(weatherDescription[0], weatherDescription[0].toUpperCase())
   const fahrenheit = Math.round((weatherData.main.temp * 1.8) + 32)
@@ -22,6 +24,51 @@ function CardExampleCard({weatherData}) {
     const longFormat = dateStamp.toLocaleString("en-US", options)
     return longFormat
   }
+
+  function dayOrNight() {
+    if (weatherData.weather[0].icon[2] === "d") {
+      setPeriodOfDay("day")
+    } else if (weatherData.weather[0].icon[2] === "n") {
+      setPeriodOfDay("night")
+    }
+  }
+
+  function setBackground() {
+    const background = document.querySelector("body")
+    const weatherCode = weatherData.weather[0].id
+
+    if (periodOfDay === "day") {
+      if (weatherCode === 800 || weatherCode === 801) {
+        background.style.backgroundColor = "#87ceeb"
+      } else if (weatherCode >= 802 && weatherCode <= 804) {
+        background.style.backgroundColor = "#A6B9C2"
+      } else if (weatherCode >= 200 && weatherCode <= 531) {
+        background.style.backgroundColor = "#7F888C"
+      } else if (weatherDescription.includes("snow") || weatherDescription.includes("sleet")) {
+        background.style.backgroundColor = "#C1CFD5"
+      }
+    }
+
+    if (periodOfDay === "night") {
+      if (weatherCode === 800 || weatherCode === 801) {
+        background.style.backgroundColor = "#12125F"
+      } else if (weatherCode >= 802 && weatherCode <= 804) {
+        background.style.backgroundColor = "#9191CE"
+      } else if (weatherCode >= 200 && weatherCode <= 531) {
+        background.style.backgroundColor = "#5B5B8E"
+      } else if (weatherDescription.includes("snow") || weatherDescription.includes("sleet")) {
+        background.style.backgroundColor = "#9191CE"
+      }
+    }
+  }
+
+  useEffect(() => {
+    dayOrNight()
+  }, [periodOfDay])
+
+  useEffect(() => {
+    if (weatherData.hasOwnProperty("weather")) { setBackground() }
+  })
 
   return (
     <Card>
