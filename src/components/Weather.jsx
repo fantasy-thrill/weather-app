20320069ff5ba1de5fe87eb6a6266c28f5d49a92
Ryan Doyley <ryan111267@gmail.com>
2023-09-06 17:20:51 -0400

    Table display

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
-import { Card } from 'semantic-ui-react'
+import { Card, CardHeader } from 'semantic-ui-react'
 
-function CardExampleCard({weatherData}) {
+function CurrentWeather({weatherData}) {
   const [periodOfDay, setPeriodOfDay] = useState("")
 
-  const weatherDescription = weatherData.weather[0].description;
-  const capitalized = weatherDescription.replace(weatherDescription[0], weatherDescription[0].toUpperCase())
+  const description = weatherData.weather[0].description;
+  const newDescription = description.replace(description[0], description[0].toUpperCase())
   const fahrenheit = Math.round((weatherData.main.temp * 1.8) + 32)
   const feelsLike = Math.round((weatherData.main.feels_like * 1.8) + 32)
   const windSpeed = Math.round(2.23694 * weatherData.wind.speed)
@@ -56,7 +56,7 @@ function CardExampleCard({weatherData}) {
         background.style.backgroundColor = "#9191CE"
       } else if (weatherCode >= 200 && weatherCode <= 531) {
         background.style.backgroundColor = "#5B5B8E"
-      } else if (weatherDescription.includes("snow") || weatherDescription.includes("sleet")) {
+      } else if (description.includes("snow") || weatherDescription.includes("sleet")) {
         background.style.backgroundColor = "#9191CE"
       }
     }
@@ -80,16 +80,29 @@ function CardExampleCard({weatherData}) {
               alt=""
               id="weather-icon" 
             />
-            <p>{capitalized}</p>
+            <p>{newDescription}</p>
           </div>
-          <h2 id="temperature">{fahrenheit + "\u00B0F"}</h2>             
-          <p>Feels Like: {feelsLike + "\u00B0F"}</p>        
-          <p>Humidity: {weatherData.main.humidity + "%"}</p>
-          <p>Wind speed: {windSpeed + " mph"}</p>
+          <h2 id="temperature">{fahrenheit + "\u00B0F"}</h2>
+          <table id='details'>
+            <tbody>
+              <tr>
+               <td className='left'>Feels Like</td>
+               <td className='right'>{feelsLike + "\u00B0F"}</td>
+              </tr>
+              <tr>
+               <td className='left'>Humidity</td>
+               <td className='right'>{weatherData.main.humidity + "%"}</td>
+              </tr>
+              <tr>
+               <td className='left'>Wind speed</td>
+               <td className='right'>{windSpeed + " mph"}</td>
+              </tr> 
+            </tbody>
+          </table>
           <p id="footer">Last updated {dateFormat(weatherData.dt)}</p>
       </Card.Content>
     </Card>
    )
 }
 
-export default CardExampleCard;
+export default CurrentWeather;
