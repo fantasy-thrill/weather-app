2423daeb95bb20093495f1e19e811c41bbd6f9b1
Ryan Doyley <ryan111267@gmail.com>
2023-09-27 13:14:08 -0400

    Working on search component

--- src/components/SearchCity.jsx
+++ src/components/SearchCity.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../App.css';
 import config from '../../config';
 import { Card } from 'semantic-ui-react'
 
 function SearchCity() {
- // const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState("")
+  const [dropdownDisplay, setDropdownDisplay] = useState("none")
+
+  const inputElem = useRef()
+  const dropdownMenu = useRef()
 
   const divStyle = {
     margin: "10px auto",
@@ -23,18 +27,32 @@ function SearchCity() {
     padding: "5px 10px"
   }
 
-  function fetchData(inputValue) {
+  const dropdownStyle = {
+    position: "absolute",
+    backgroundColor: "silver",
+    border: "1px solid",
+    display: dropdownDisplay,
+  }
+
+  function fetchData() {
     const capitalLetter = inputValue[0].toUpperCase()
     const capInputValue = inputValue.replace(inputValue[0], capitalLetter)
 
     fetch(`${config.geoApiURL}/direct?q=${capInputValue}&limit=5&appid=${config.apiKey}`)
       .then(result => result.json())
-      .then()
+      .then(cities => {
+        if (cities.length > 0) {
+          setDropdownDisplay("block")
+          return cities.map(city => 
+            dropdownMenu.current.innerHTML = `<div>${city.name}, ${city.state} ${city.country}</div>`)
+        }
+      })
   }
 
   return (
     <div style={divStyle}>
-      <input type="text" style={inputStyle}/>
+      <input type="text" style={inputStyle} ref={inputElem} onInput={() => setInputValue()}/>
+      <div id="dropdown-menu" style={dropdownStyle} ref={dropdownMenu}></div>
       <button type="submit" style={buttonStyle}>Search</button>
       <p>Click <Link to="/current">here</Link> to return to main page.</p>
     </div>
