d4fb17d59bb614ade920cf11ed3b4c86b90ff775
Ryan Doyley <ryan111267@gmail.com>
2024-12-06 08:46:52 -0500

    Compiled main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../build/src/App'
+import App from './App'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot((document.getElementById('root')) as Element | DocumentFragment).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
