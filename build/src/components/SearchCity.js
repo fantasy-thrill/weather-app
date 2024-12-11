331da9096d93425a2f1b82b230c79426b9c2e400
Ryan Doyley <ryan111267@gmail.com>
2024-12-10 20:34:16 -0500

    Fixed API call

--- build/src/components/SearchCity.js
+++ build/src/components/SearchCity.js
@@ -6,7 +6,7 @@ function SearchCity() {
     const dropdownMenu = useRef(null);
     const navigate = useNavigate();
     function fetchData(inputValue) {
-        fetch(`http://api.geonames.org/searchJSON?q=united+states&name_startsWith=${inputValue}&maxRows=10&username=${config.geoApiUsername}`)
+        fetch(`/api/searchJSON?q=united+states&name_startsWith=${inputValue}&maxRows=10&username=${config.geoApiUsername}`)
             .then(result => result.json())
             .then(cities => {
             if (dropdownMenu.current) {
